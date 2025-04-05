49e41c1aabb3d1e7ba0c66871c54dd78c5d2a74d
Rielly Kirk <Riellykirk13@gmail.com>
2025-04-05 12:31:06 -0400

    Step 6 lazy loader is done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { provideTranslocoScope } from '@jsverse/transloco';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('./about/about.component').then((m) => m.AboutComponent),
+    providers: [provideTranslocoScope('about')] // lazy load 'about' scope
+  },
+  {
+    path: '**', redirectTo: '/'
+  }
+];
