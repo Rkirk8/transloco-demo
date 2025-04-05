8cf10d11b03759da3235c33a8079407c04b16141
Rielly Kirk <Riellykirk13@gmail.com>
2025-04-05 11:10:13 -0400

    Step 3 Added lang swither with the english in the Json

--- src/app/language-switcher/language-switcher.component.ts
+++ src/app/language-switcher/language-switcher.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { TranslocoService, TranslocoDirective } from "@jsverse/transloco";
+import { NgFor } from "@angular/common";
 
 @Component({
-  selector: 'app-language-switcher',
-  imports: [],
-  templateUrl: './language-switcher.component.html',
-  styleUrl: './language-switcher.component.css'
+  selector: "app-language-switcher",
+  standalone: true,
+  imports: [NgFor, TranslocoDirective],
+  templateUrl: `./language-switcher.component.html`
 })
 export class LanguageSwitcherComponent {
+  currentLang: string; // stores the current language
+  langs: string[]; // stores the available languages
 
+  constructor(private translocoService: TranslocoService) {
+    this.currentLang = this.translocoService.getActiveLang();
+    // get all available languages
+    const availableLangs = this.translocoService.getAvailableLangs();
+    // check if langs are provided as string or object
+    if (Array.isArray(availableLangs) && typeof availableLangs[0] === "string") {
+      this.langs = availableLangs as string[];
+    } else {
+      this.langs = (availableLangs as { id: string; label: string }[]).map(lang => lang.id);
+    }
+  }
+  
+  // on change event get HTML select element value and set active lang
+  onChange(event: Event): void {
+    this.translocoService.setActiveLang((event.target as HTMLSelectElement).value);
+  }
 }
