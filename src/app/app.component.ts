e345ee9228218c8c1c4296b241b7853d4bef4d30
Rielly Kirk <Riellykirk13@gmail.com>
2025-04-05 11:43:05 -0400

    Step 4 tranloco works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TranslocoDirective } from '@jsverse/transloco'; 
+import { TranslocoDirective } from '@jsverse/transloco';
+// add the langage switcher component
+import { LanguageSwitcherComponent } from './language-switcher/language-switcher.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TranslocoDirective],
+  imports: [RouterOutlet, TranslocoDirective, LanguageSwitcherComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
